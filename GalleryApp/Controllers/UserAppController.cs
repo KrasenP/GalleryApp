5af71246f3c7eb2f95b68af96367ad04a5098ebf
KrasenPenev <krasennikolicov@abv.bg>
2024-01-13 20:43:47 +0200

    Succsesfull scafold register and login

--- GalleryApp/Controllers/UserAppController.cs
+++ GalleryApp/Controllers/UserAppController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GalleryApp.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GalleryApp.Controllers
 {
     public class UserAppController : Controller
     {
+        private readonly SignInManager<UserApp> _signInManager;
+        private readonly UserManager<UserApp> _userManager;
 
-
+        public IActionResult Register() 
+        {
+            return View();
+        }
 
         public IActionResult AddToAlbum()
         {
